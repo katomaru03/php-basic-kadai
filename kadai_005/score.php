3ff763165c60684e435ed47eff68e0da30484daa
katomaru03 <yuka0583160@gmail.com>
2025-01-05 22:55:42 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>kadai_005</title>
+ </head>
+
+ <body>
+    <p>
+        <?php
+        $user_name = 'score1 = 80' ;
+        echo $user_name;
+        ?>
+    </P>
+
+    <p>
+        <?php
+        $user_name = 'score2 = 60' ;
+        echo $user_name;
+        ?>
+    </P>
+
+    <p>
+        <?php
+        $user_name = 'score3 = 55' ;
+        echo $user_name;
+        ?>
+    </P>
+
+    <p>
+        <?php
+        $user_name = 'score4 = 40' ;
+        echo $user_name;
+        ?>
+    </P>
+
+    <p>
+        <?php
+        $user_name = 'score5 = 100' ;
+        echo $user_name;
+        ?>
+    </P>
+
+    <p>
+        <?php
+        $user_name = 'score6 = 25' ;
+        echo $user_name;
+        ?>
+    </P>
+
+    <p>
+        <?php
+        $user_name = 'score7 = 80' ;
+        echo $user_name;
+        ?>
+    </P>
+
+    <p>
+        <?php
+        $user_name = 'score8 = 95' ;
+        echo $user_name;
+        ?>
+    </P>
+
+    <p>
+        <?php
+        $user_name = 'score9 = 30' ;
+        echo $user_name;
+        ?>
+    </P>
+
+    <p>
+        <?php
+        $user_name = 'score10 = 60' ;
+        echo $user_name;
+        ?>
+    </P>
+
+    <p>
+        <?php
+        echo 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+        echo '<br>';
+        ?>
+    </p> 
+
+    <p>
+        <?php 
+        echo 625 / 10 ;
+        echo '<br>';
+        ?>
+    </p>
+    
+</body>
+
+ </html>
